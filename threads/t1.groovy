b470510fb7d2054a5a32e2df738bac28a28f13dd
goel.piyush84@gmail.com <piyush.goel@inmobi.com>
2015-10-22 19:35:49 +0530

    ...

--- threads/t1.groovy
+++ threads/t1.groovy
@@ -1,5 +1,9 @@
+println("starting the program");
+
 ["one","two","three","four"].each { tid ->
     Thread.start {
         println "Thread $tid says Hello World!"
-    }
+   	Thread.sleep(5000)
+	println "Sleet completed: $tid"
+	}
 }
